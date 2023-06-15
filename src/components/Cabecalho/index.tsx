53b884602b69c8b9959b2a54d331229a4613f03d
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-06-15 12:42:01 -0300

    changes

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,9 @@
-import styles from './Cabecalho.module.css'
+import { HeaderModule } from './styles'
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
+  <HeaderModule>
     <h1>EBAC Jobs</h1>
-  </header>
+  </HeaderModule>
 )
 
 export default Cabecalho
--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import { VagasButton, VagasForm, VagasInput } from './style'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <VagasForm onSubmit={aoEnviarForm}>
+      <VagasInput
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <VagasButton type="submit">Pesquisar</VagasButton>
+    </VagasForm>
   )
 }
 export default FormVagas
